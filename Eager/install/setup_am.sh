4ce93f655a29235393c94cfdfe2cd5514ea73ae2
Hiranya Jayathilaka <hiranya911@gmail.com>
2013-12-22 14:47:27 -0800

    Minor improvement to the setup script

--- Eager/install/setup_am.sh
+++ Eager/install/setup_am.sh
@@ -4,8 +4,10 @@ echo "====================================="
 
 apt-get install -y unzip
 rm -rfv /root/APIManager
-unzip $1 -d /root
-mv /root/wso2am-* /root/APIManager
+mkdir -p /root/temp
+unzip $1 -d /root/temp
+mv /root/temp/wso2am-* /root/APIManager
+rm -rfv /root/temp
 
 echo "Creating WSO2CARBON_DB database tables..."
 mysql -uroot -p$2 WSO2CARBON_DB < /root/APIManager/dbscripts/mysql.sql
