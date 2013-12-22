ff5aabd6f3b9f600e8f209fc5b19a0efcb01de2b
Hiranya Jayathilaka <hiranya911@gmail.com>
2013-12-22 00:08:31 -0800

    fixed incorrect syntax

--- Eager/install/setup_mysql.sh
+++ Eager/install/setup_mysql.sh
@@ -21,7 +21,7 @@ echo "Creating databases and user accounts."
 mysql -uroot -p$1 -e <<EOSQL "DROP DATABASE IF EXISTS WSO2_CARBON_DB; CREATE DATABASE WSO2_CARBON_DB; GRANT ALL PRIVILEGES ON WSO2_CARBON_DB.* TO $2@localhost IDENTIFIED BY '$3'"
 EOSQL
 RC=$?
-if [[ $RC != 0 ]]; then
+if [ $RC != 0 ]; then
     exit $RC
 fi
 echo "Created database WSO2_CARBON_DB for user $2"
@@ -29,7 +29,7 @@ echo "Created database WSO2_CARBON_DB for user $2"
 mysql -uroot -p$1 -e <<EOSQL "DROP DATABASE IF EXISTS WSO2_AM_DB; CREATE DATABASE WSO2_AM_DB; GRANT ALL PRIVILEGES ON WSO2_AM_DB.* TO $2@localhost IDENTIFIED BY '$3'"
 EOSQL
 RC=$?
-if [[ $RC != 0 ]]; then
+if [ $RC != 0 ]; then
     exit $RC
 fi
 echo "Created database WSO2_AM_DB for user $2"
