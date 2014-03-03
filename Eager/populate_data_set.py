f0b50fd1fe108898ea2c2f2b6ef4f43d25c183d2
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-03-02 17:57:37 -0800

    escaping more special chars

--- Eager/populate_data_set.py
+++ Eager/populate_data_set.py
@@ -54,7 +54,7 @@ def add_mashup_set(lines, eager, secret, spec):
   for k in sorted(mashups.keys()):
     v = mashups[k]
     api_name = k.lower()
-    for char in "'/ &+*@%\"<>!,":
+    for char in "'/ &+*@%\"<>!,#;^(){}|~":
       api_name = api_name.replace(char, '')
     api_name = 'm{0}_{1}'.format(count, api_name)
     spec['apiName'] = api_name
