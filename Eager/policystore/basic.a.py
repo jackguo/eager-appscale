4113d5f7408beadd46a93c964a9ce7d47c7cb0b8
Junsheng <root@appscale-image0.(none)>
2014-06-03 03:57:19 +0000

    added sample policy

---
+++ Eager/policystore/basic.a.py
@@ -0,0 +1,13 @@
+import re
+
+'''
+regex = re.compile('[A-Z].*')
+assert_true(regex.match(app.name), 'Application names must start with an uppercase letter')
+'''
+
+regex = re.compile('admin@test.com')
+assert_true(regex.match(app.owner), 'Application username must match admin@test.com')
+
+regex = re.compile('[A-Z].*')
+for api in app.api_list:
+  assert_true(regex.match(api.name), 'API names must start with an uppercase letter')
