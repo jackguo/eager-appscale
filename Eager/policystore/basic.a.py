f29a8eba5b582f7d43294a2abb84ae8daca856f2
Junsheng <root@appscale-image0.(none)>
2014-06-03 04:02:04 +0000

    ...

--- Eager/policystore/basic.a.py
+++ Eager/policystore/basic.a.py
@@ -1,9 +1,7 @@
 import re
 
-'''
 regex = re.compile('[A-Z].*')
 assert_true(regex.match(app.name), 'Application names must start with an uppercase letter')
-'''
 
 regex = re.compile('admin@test.com')
 assert_true(regex.match(app.owner), 'Application username must match admin@test.com')
